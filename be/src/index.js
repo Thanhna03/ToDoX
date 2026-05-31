1a66f9022b456fb8979a8642c338e07d20dd71e9
khuongthanhtam712 <khuongthanhtam712@gmail.com>
2026-05-31 11:47:55 +0700

    chinh file index.js

--- be/src/index.js
+++ be/src/index.js
@@ -1,19 +1,19 @@
-import 'dotenv/config';
+import "dotenv/config";
 import express, { response } from "express";
-import taskRoute from './routes/taskRouters.js'
+import taskRoute from "./routes/taskRouters.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5001 //neu env kh cos port -> dung gia tri mac dinh 5001
+const PORT = process.env.PORT || 5001; //neu env kh cos port -> dung gia tri mac dinh 5001
 
 const app = express();
 
-connectDB();
+app.use("/api/tasks", taskRoute);
 
-app.use("/api/tasks", taskRoute)
-
-app.listen(PORT, () => {
+connectDB().then( () => { //lien ket database -> server moi duoc chay
+    app.listen(PORT, () => {
     console.log(`server dang bat dau tren cong ${PORT}`);
-})
+  });
+});
